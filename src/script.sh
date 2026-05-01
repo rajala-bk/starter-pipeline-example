2aa26454240301a26f920127dabb12d2e96ae0f0
rajala-bk <richard.ajala@buildkite.com>
2026-05-01 14:17:48 -0600

    passing script with if commented out

--- src/script.sh
+++ src/script.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-echo "Running script..."
-echo "Intentionally failing for notification test."
-exit 1
+# echo "Running script..."
+# echo "Intentionally failing for notification test."
+# exit 1
+
+echo "Script completed successfully."
+exit 0
